18a88492cf2d115911eaea7287191cc98b4321f2
Daniel Olano <daniel@olanod.com>
2020-05-16 10:33:25 +0200

    Add logging

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 use futures::io;
-use libp2p::{identity, mdns::Mdns, swarm::Swarm};
+use libp2p::{
+    identity,
+    mdns::Mdns,
+    swarm::{Swarm, SwarmEvent},
+};
+use log::{debug, info};
 use std::{
     error::Error,
     pin::Pin,
@@ -11,6 +16,7 @@ pub struct PeerPipe(Swarm<Mdns>);
 impl PeerPipe {
     pub fn new() -> Result<Self, Box<dyn Error>> {
         let id_keys = identity::Keypair::generate_ed25519();
+        debug!("Generated peer keys {}", id_keys.public().into_peer_id());
         Self::new_peer(id_keys)
     }
 
@@ -18,30 +24,32 @@ impl PeerPipe {
         let peer_id = keys.public().into_peer_id();
         let transport = libp2p::build_tcp_ws_secio_mplex_yamux(keys)?;
         let behaviour = Mdns::new()?;
-        let mut swarm = Swarm::new(transport, behaviour, peer_id);
-        Swarm::listen_on(&mut swarm, "/ip4/0.0.0.0/tcp/0".parse()?)?;
-        // loop {
-        //     let event = swarm.next_event().await;
-        //     match event {
-        //         SwarmEvent::NewListenAddr(addr) => println!("Listening on {}", addr),
-        //         _ => println!("{:?}", event),
-        //     }
-        // }
-
+        let swarm = Swarm::new(transport, behaviour, peer_id);
         Ok(Self(swarm))
+    }
+
+    pub async fn connect(&mut self, _reader: impl io::AsyncRead) -> Result<(), Box<dyn Error>> {
+        Swarm::listen_on(&mut self.0, "/ip4/0.0.0.0/tcp/0".parse()?)?;
+        loop {
+            let event = self.0.next_event().await;
+            match event {
+                SwarmEvent::NewListenAddr(addr) => info!("Listening on {}", addr),
+                _ => debug!("{:?}", event),
+            }
+        }
     }
 }
 
 impl io::AsyncWrite for PeerPipe {
-    fn poll_write(self: Pin<&mut Self>, cx: &mut Context, buf: &[u8]) -> Poll<io::Result<usize>> {
+    fn poll_write(self: Pin<&mut Self>, _cx: &mut Context, _buf: &[u8]) -> Poll<io::Result<usize>> {
         Poll::Ready(Ok(0))
     }
 
-    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context) -> Poll<io::Result<()>> {
+    fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context) -> Poll<io::Result<()>> {
         Poll::Ready(Ok(()))
     }
 
-    fn poll_close(self: Pin<&mut Self>, cx: &mut Context) -> Poll<io::Result<()>> {
+    fn poll_close(self: Pin<&mut Self>, _cx: &mut Context) -> Poll<io::Result<()>> {
         Poll::Ready(Ok(()))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use async_std::io::stdin;
-use futures::io;
 use peer_piper::PeerPipe;
 use std::error::Error;
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let mut link = PeerPipe::new()?;
-    io::copy(stdin(), &mut link).await?;
-    Ok(())
+    simple_logger::init()?;
+    PeerPipe::new()?.connect(stdin()).await
 }
